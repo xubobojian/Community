f2df51b0ccc0c7dbc602e879f4714e04996876b1
xubobojian <ab12345678c@sina.com>
2022-03-10 14:59:50 +0800

    第一次提交

---
+++ src/main/java/com/bobo_life_community/Application.java
@@ -0,0 +1,13 @@
+package com.bobo_life_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
